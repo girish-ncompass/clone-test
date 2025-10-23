c2185f74c4f880495ef9de16863480d5be5c35a5
Gary <gary@ncompass.inc>
2025-10-23 17:19:29 +0530

    feat: added navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,5 +1,57 @@
+import { AccountCircle, Notifications,  Search } from "@mui/icons-material";
+import { AppBar, Box,  TextField, Toolbar, Typography } from "@mui/material";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return<>
+    <AppBar>
+      <Toolbar>
+       <Typography>
+        NCompass
+       </Typography>
+       
+        <Box 
+        display={"flex"}
+        alignItems={"center"}
+        sx={{
+           backgroundColor:'secondary.main',
+           borderRadius:'50px',
+           width: 500,
+           margin: "5px",
+          padding:"6px 6px"
+            }}
+        >
+          <Search sx={{color:"black"}}/>
+        <TextField
+        fullWidth
+          variant="standard"
+          placeholder="Search employees or actions (Ex: Apply Leave)"
+          // size="small"
+          slotProps={{  input: {
+            disableUnderline:true
+          } }}
+          
+          sx={{  
+            '.MuiInputBase-input': { 
+          fontSize: '12px', // Adjust as needed
+          color:"white",
+          fontWeight:"extraLight"
+        }, 
+          }}
+          
+        />
+        </Box>
+          <Box
+            sx={{
+              marginLeft:"auto"
+            }}
+          >
+            <Notifications fontSize="medium"/>
+          <AccountCircle fontSize="medium"/>
+          </Box>
+       
+      </Toolbar>
+    </AppBar>;
+    </>
 }
 
 export default Navbar;
